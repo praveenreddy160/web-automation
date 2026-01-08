cfb9e8e62dede89d5dbb6fc7889210b140edcefc
Praveen Reddy Peddi <praveenreddy160@gmail.com>
2026-01-08 13:04:40 -0800

    added selenium code

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 chrome_options = webdriver.ChromeOptions()
 # The 'detach' option keeps the browser open after the script finishes
 chrome_options.add_experimental_option("detach", True)
-print(chrome_options)
 
 # This calls the Chrome class constructor (__init__) with our options to create the driver object
 driver = webdriver.Chrome(options=chrome_options)
@@ -12,8 +11,9 @@ driver.implicitly_wait(5)
 driver.get('https://www.amazon.com/gp/product/B09231NL5H/ref=ox_sc_act_title_1?smid=A21U86ONRH0Q7Z&psc=1#averageCustomerReviewsAnchor')
 
 # Use the find_element method of the driver object to locate HTML elements by their class name
-price_dollar = driver.find_element(By.CLASS_NAME, "a-price-symbol")
-price_cents = driver.find_element(By.CLASS_NAME, "a-price-whole")
-print(price_dollar.text + price_cents.text)
+price_symbol = driver.find_element(By.CLASS_NAME, "a-price-symbol")
+price_whole = driver.find_element(By.CLASS_NAME, "a-price-whole")
+price_fraction = driver.find_element(By.CLASS_NAME, "a-price-fraction")
+print(f"The price of the item is {price_symbol.text}{price_whole.text}.{price_fraction.text}")
 # driver.close()
-driver.quit()
+# driver.quit()
